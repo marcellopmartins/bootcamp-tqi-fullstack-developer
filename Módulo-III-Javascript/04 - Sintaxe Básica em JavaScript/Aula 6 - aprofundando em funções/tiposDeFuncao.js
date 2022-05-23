39964426f88aff5afa44fa1e0c258edde01ea40c
marcellopmartins <marcellopmartins@gmail.com>
2022-05-23 15:48:45 -0300

    Sintaxe Básica em JavaScript e projeto calculadora.

--- Módulo-III-Javascript/04 - Sintaxe Básica em JavaScript/Aula 6 - aprofundando em funções/tiposDeFuncao.js
+++ Módulo-III-Javascript/04 - Sintaxe Básica em JavaScript/Aula 6 - aprofundando em funções/tiposDeFuncao.js
@@ -4,13 +4,13 @@
 // deve ser declarada usando a palavra reservada function seguida do nome da funçao, parênteses "()" e chaves "{}"
 // deve ser nomeada obrigatioriamente
 
-/* function nomeDaFuncao() {
+ function nomeDaFuncao() {
     alert('Sou uma função declarativa!');
 }
 
 // devemos chamar a função para ver o resultado
 // chamando a função
-nomeDaFuncao(); */
+nomeDaFuncao(); 
 
 
 // expressão de função
@@ -18,22 +18,22 @@ nomeDaFuncao(); */
 // a nomeação é opcional
 
 // com nomeação
-/* var funcao = function funcao() {
+ var funcao = function funcao() {
     alert('Sou uma expressão de função nomeada!');
-} */
+} 
 
 // sem nomeação
-/* var funcao = function() {
+ var funcao = function() {
     alert('Sou uma expressão de função não nomeada!')
 }
-*/
+
 
 // arrow Function
 // função de expressão de sintaxe curta
 // sempre anônima, não pode ser nomeada
 // deve ser declarada com parênteses "()", seguido de "=>" e depois chaves "{}"
-/* var funcao = () => {
+ var funcao = () => {
     alert('Sou uma função anônima!')
 };
 
-funcao(); */
+funcao(); 
